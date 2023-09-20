d97975b5ed0016a9a49029514ac7d03b36d530d6
YEK-Kayra <124110070+YEK-Kayra@users.noreply.github.com>
2023-09-20 13:20:37 +0300

    BMP280_Driver_STM32
    It contains driver files and application for STM32 users. It support I2C interface in communication now

---
+++ BMP280_DriverFiles/BMP280.c
@@ -0,0 +1,223 @@
+/*!
+ *  @file : BMP280.c
+ *  @date : 8-10-2023
+ *  @version : v1.0.0
+ *
+ *      Author: Yunus Emre KAYRA (https://github.com/YEK-Kayra)
+ ******************************************************************************
+ * 	@attention
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023 TAISAT Turkish Artificial Intelligence Supported Autonomous Technologies
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+
+/****************************************************************/
+/*! @name      	 Header includes                */
+/****************************************************************/
+#include "BMP280.h"
+#include "stm32f1xx.h"
+#include "main.h"
+#include "stdint.h"
+#include "math.h"
+
+
+float FixedAltitude;
+float TemporaryAltitude;
+
+/******************************************************************************/
+/*! @name        BMP280  Function prototypes            				*/
+/******************************************************************************/
+
+
+void BMP280_Init(BMP280_HandleTypeDef *BMP280){
+
+
+
+	BMP280_Get_CalibrationDatas(BMP280);
+
+	BMP280->BMP280_CtrlMeas_Params_t = (BMP280->BMP280_Params.Oversampling_Temperature<<5) |
+			   	   	   	   	   	   	   (BMP280->BMP280_Params.Oversampling_Pressure<<2) |
+									   (BMP280->BMP280_Params.Mode<<0);
+
+
+    BMP280->BMP280_Config_Params_t 	 = (BMP280->BMP280_Params.StandbyTime<<5) |
+			   	   	   	   	   	   	   (BMP280->BMP280_Params.Filter<<2) |
+									   (BMP280->BMP280_Params.CommunicationMode<<0);
+
+
+    HAL_I2C_Mem_Write(BMP280->i2c, BMP280->BMP280_I2C_ADDRESS, BMP280_CtrlMeas_REG, 1, &BMP280->BMP280_CtrlMeas_Params_t, 1, 1000);
+    HAL_I2C_Mem_Write(BMP280->i2c, BMP280->BMP280_I2C_ADDRESS, BMP280_Config_REG, 1, &BMP280->BMP280_Config_Params_t, 1, 1000);
+
+    for(int cnt = 0 ; cnt < 20 ; cnt++){
+
+    	BMP280_GetVal_TemperatureAndPressure(BMP280, &BMP280_Temperature, &BMP280_Pressure, &BMP280_Altitude);
+    	TemporaryAltitude = (float)(TemporaryAltitude + (float)(BMP280_Altitude * (0.05)));
+
+    }
+
+    FixedAltitude = TemporaryAltitude;
+}
+
+
+void BMP280_Get_CalibrationDatas(BMP280_HandleTypeDef *BMP280){
+
+	uint8_t BMP280_CalibrationDatas[24] = {0};
+	uint8_t CNT = 0;
+
+	if(HAL_I2C_IsDeviceReady(BMP280->i2c, BMP280->BMP280_I2C_ADDRESS, 1, 500) != HAL_OK){
+
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET);
+
+	}
+
+	HAL_I2C_Mem_Read(BMP280->i2c, BMP280->BMP280_I2C_ADDRESS, Calibration_Address_Start, 1, &BMP280_CalibrationDatas[0], 24, 1000);
+
+	BMP280->dig_T1 = (uint16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_T2 =  (int16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_T3 =  (int16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_P1 = (uint16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_P2 =  (int16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_P3 =  (int16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_P4 =  (int16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_P5 =  (int16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_P6 =  (int16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_P7 =  (int16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_P8 =  (int16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+	BMP280->dig_P9 =  (int16_t)((BMP280_CalibrationDatas[CNT]) | (BMP280_CalibrationDatas[CNT+1]<<8)); CNT+=2;
+
+}
+
+
+void BMP280_Set_DefaultParams(BMP280_Params_t *BMP280_Params){
+
+	BMP280_Params->CommunicationMode = BMP280_I2C;
+	BMP280_Params->Filter = BMP280_Filter_X16;
+	BMP280_Params->StandbyTime = BMP280_Standby_05;
+	BMP280_Params->Mode = BMP280_Mode_Normal;
+	BMP280_Params->Oversampling_Pressure = BMP280_Ultra_High_Resolution;
+	BMP280_Params->Oversampling_Temperature = BMP280_Ultra_High_Resolution;
+
+}
+
+
+bool BMP280_GetVal_TemperatureAndPressure(BMP280_HandleTypeDef *BMP280, double *BMP280_Temperature, double *BMP280_Pressure, float *BMP280_Altitude){
+
+	uint8_t PressTemp_MSB_LSB_XLSB[6] = {0};
+	int32_t RawTemperature;
+	int32_t RawPressure;
+	int32_t TempVarTotel;
+
+
+	if(BMP280_Control_Status(BMP280) == true){
+
+
+		HAL_I2C_Mem_Read(BMP280->i2c, BMP280->BMP280_I2C_ADDRESS, RawDataRegStartAddress, 1, PressTemp_MSB_LSB_XLSB, 6, 1000);
+
+		RawTemperature = (PressTemp_MSB_LSB_XLSB[3]<<12 | PressTemp_MSB_LSB_XLSB[4]<<4 | PressTemp_MSB_LSB_XLSB[5]>>4);
+		RawPressure = (PressTemp_MSB_LSB_XLSB[0]<<12 | PressTemp_MSB_LSB_XLSB[1]<<4 | PressTemp_MSB_LSB_XLSB[2]>>4);
+
+
+		*BMP280_Temperature = (float)BMP280_Calculate_CompensatedTemperature(BMP280, RawTemperature,&TempVarTotel);
+		*BMP280_Pressure = (float)BMP280_Calculate_CompensatedPressure(BMP280, RawPressure, TempVarTotel);
+		*BMP280_Altitude = (float)BMP280_Calculate_Altitude(BMP280_Pressure);
+
+		return 1;
+
+	}
+	else{
+
+		return 0;
+
+	}
+
+}
+
+
+float BMP280_Calculate_Altitude(double *BMP280_Pressure){
+
+	return ((SeaLevelTemp / GradientTemp) * (1 - pow((*BMP280_Pressure / SeaLevelPress),((GasCoefficient * GradientTemp)/GravityAccel)))-FixedAltitude);
+
+}
+
+
+double BMP280_Calculate_CompensatedTemperature(BMP280_HandleTypeDef *BMP280, int32_t RawTemperature, int32_t *TempVarTotel){
+
+	int32_t var1, var2;
+
+	var1 = ((((RawTemperature >> 3) - ((int32_t) BMP280->dig_T1 << 1)))
+			* (int32_t) BMP280->dig_T2) >> 11;
+
+	var2 = (((((RawTemperature >> 4) - (int32_t) BMP280->dig_T1)
+			* ((RawTemperature >> 4) - (int32_t) BMP280->dig_T1)) >> 12)
+			* (int32_t) BMP280->dig_T3) >> 14;
+
+	*TempVarTotel = var1 + var2;
+	return ((float)((*TempVarTotel * 5 + 128) >> 8)/100);
+
+}
+
+
+double BMP280_Calculate_CompensatedPressure(BMP280_HandleTypeDef *BMP280, int32_t RawPressure,int32_t TempVarTotel){
+
+	int64_t var1, var2, p;
+
+	var1 = (int64_t) TempVarTotel - 128000;
+	var2 = var1 * var1 * (int64_t) BMP280->dig_P6;
+	var2 = var2 + ((var1 * (int64_t) BMP280->dig_P5) << 17);
+	var2 = var2 + (((int64_t) BMP280->dig_P4) << 35);
+	var1 = ((var1 * var1 * (int64_t) BMP280->dig_P3) >> 8)
+			+ ((var1 * (int64_t) BMP280->dig_P2) << 12);
+	var1 = (((int64_t) 1 << 47) + var1) * ((int64_t) BMP280->dig_P1) >> 33;
+
+	if (var1 == 0) {
+		return 0;  // avoid exception caused by division by zero
+	}
+
+	p = 1048576 - RawPressure;
+	p = (((p << 31) - var2) * 3125) / var1;
+	var1 = ((int64_t) BMP280->dig_P9 * (p >> 13) * (p >> 13)) >> 25;
+	var2 = ((int64_t) BMP280->dig_P8 * p) >> 19;
+
+	p = ((p + var1 + var2) >> 8) + ((int64_t) BMP280->dig_P7 << 4);
+	return ((float)p/256);
+
+}
+
+
+bool BMP280_Control_Status(BMP280_HandleTypeDef *BMP280){
+
+	HAL_I2C_Mem_Read(BMP280->i2c, BMP280->BMP280_I2C_ADDRESS, BMP280_Status_REG, 1, &BMP280->BMP280_Status, 1, 1000);
+
+
+	while(!(BMP280->BMP280_Status & 0x01)){
+
+		HAL_I2C_Mem_Read(BMP280->i2c, BMP280->BMP280_I2C_ADDRESS, BMP280_Status_REG, 1, &BMP280->BMP280_Status, 1, 1000);
+
+	}
+
+	return true;
+
+}
+
---
+++ BMP280_DriverFiles/BMP280.h
@@ -0,0 +1,238 @@
+/*!
+ * BMP280.h
+ *
+ * Author: Yunus Emre KAYRA (github.com/YEK-Kayra)
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023 TAISAT Turkish Artificial Intelligence Supported Autonomous Technologies
+ *
+ */
+
+#ifndef INC_BMP280_H_
+#define INC_BMP280_H_
+#define BMP280_DEV
+
+/****************************************************************/
+/*! @name       Header includes             */
+/****************************************************************/
+#include "main.h"
+#include "stdbool.h"
+#include "stdint.h"
+
+/****************************************************************/
+/*! @name       General Macro Definitions             */
+/****************************************************************/
+
+/**!These macros provide to access the registers of BMP280 */
+#define Calibration_Address_Start  0x88
+#define RawDataRegStartAddress	   0xF7
+#define TempRegStartAddress		   0xFA
+#define BMP280_Config_REG		   0xF5 	/*! bits: t_sb[7:5] ,filter[4:2] ,spi3w_en[0]  		   */
+#define BMP280_CtrlMeas_REG		   0xF4 	/*! bits: osrs_t[7:5] ,osrs_t[4:2] ,mode[1:0] 		   */
+#define BMP280_Status_REG		   0xF3 	/*! bits: measuring[3] ,im_update[0]  		    	   */
+#define BMP280_Reset_REG		   0xE0		/*! bits: reset[7:0] write 0xB6 into the reg for reset */
+
+/**!These macros provide to calculate the altitude of BMP280 */
+#define SeaLevelPress  101325
+#define SeaLevelTemp   288.15
+#define GradientTemp   0.0065
+#define GravityAccel   9.80665
+#define GasCoefficient 287.05
+
+/******************************************************************************/
+/*!  @name         External Variable Declarations                                  */
+/******************************************************************************/
+extern double BMP280_Pressure;
+extern double BMP280_Temperature;
+extern float  BMP280_Altitude;
+
+/******************************************************************************/
+/*!  @name         Enum Declarations                                  */
+/******************************************************************************/
+typedef enum {
+
+	BMP280_Mode_Sleep  = 0,   /* no measurement          */
+	BMP280_Mode_Forced = 1,   /* single measurement      */
+	BMP280_Mode_Normal = 3	  /* continuous measurement  */
+
+}BMP280_Mode;
+
+
+typedef enum{
+
+	BMP280_I2C = 0,			/* Select I2C protocol for communication */
+	BMP280_SPI = 1			/* Select SPI protocol for communication */
+
+}BMP280_Communication;
+
+
+typedef enum {
+
+	BMP280_Skipped = 0,                 			/* no measurement */
+	BMP280_Ultra_Low_Power_Resolution = 1,         	/* oversampling x1 */
+	BMP280_Low_Power_Resolution = 2,				/* oversampling x2 */
+	BMP280_Standart_Resolution = 3, 				/* oversampling x4 */
+	BMP280_High_Resolution = 4,						/* oversampling x8 */
+	BMP280_Ultra_High_Resolution = 5 				/* oversampling x16 */
+
+}BMP280_Oversampling;
+
+
+typedef enum {
+
+	BMP280_Filter_OFF = 0,      /* no filter */
+	BMP280_Filter_X2  = 1,      /* filter coefficient : 2 */
+	BMP280_Filter_X4  = 2,		/* filter coefficient : 4 */
+	BMP280_Filter_X8  = 3,		/* filter coefficient : 8 */
+	BMP280_Filter_X16 = 4		/* filter coefficient : 16 */
+
+}BMP280_Filter;
+
+
+typedef enum {
+
+	BMP280_Standby_05  = 0,         /* 0.5ms standby*/
+	BMP280_Standby_62  = 1,         /* 62.5ms standby*/
+	BMP280_Standby_125 = 2,         /* 125ms standby*/
+	BMP280_Standby_250 = 3,         /* 250ms standby*/
+	BMP280_Standby_500 = 4,         /* 500ms standby*/
+	BMP280_Standby_1000 = 5,        /* 1000ms standby*/
+	BMP280_Standby_2000 = 6,        /* 2000ms standby*/
+	BMP280_Standby_4000 = 7         /* 4000ms standby*/
+
+}BMP280_StandbyTime;
+
+
+/******************************************************************************/
+/*!  @name         Structure Declarations                             */
+/******************************************************************************/
+typedef struct {
+
+	BMP280_Mode   Mode;
+	BMP280_Oversampling Oversampling_Pressure;
+	BMP280_Oversampling Oversampling_Temperature;
+	BMP280_Filter Filter;
+	BMP280_StandbyTime StandbyTime;
+	BMP280_Communication CommunicationMode;
+
+}BMP280_Params_t;
+
+typedef struct {
+
+	uint16_t dig_T1;
+	int16_t  dig_T2;
+	int16_t  dig_T3;
+	uint16_t dig_P1;
+	int16_t dig_P2;
+	int16_t dig_P3;
+	int16_t dig_P4;
+	int16_t dig_P5;
+	int16_t dig_P6;
+	int16_t dig_P7;
+	int16_t dig_P8;
+	int16_t dig_P9;
+
+	I2C_HandleTypeDef *i2c;
+
+	BMP280_Params_t BMP280_Params;
+
+	uint16_t BMP280_I2C_ADDRESS;
+
+	uint8_t  BMP280_Config_Params_t;
+	uint8_t	 BMP280_CtrlMeas_Params_t;
+    uint8_t  BMP280_Status;
+
+
+
+}BMP280_HandleTypeDef;
+
+
+/******************************************************************************/
+/*         			BMP280 Function prototypes            				*/
+/******************************************************************************/
+
+/**
+ * @fn      void BMP280_Init(BMP280_HandleTypeDef *BMP280)
+ * @brief   Get calibration datas from BMP280's chip
+ * @param   *BMP280
+ * @return  None
+ */
+void BMP280_Get_CalibrationDatas(BMP280_HandleTypeDef *BMP280);
+
+
+/**
+ * @fn      void BMP280_Set_DefaultParams(BMP280_Params_t *BMP280_Params)
+ * @brief   Select BMP280 configurations. The values can be changed by users
+ * @param   *BMP280_Params
+ * @retval  None
+ */
+void BMP280_Set_DefaultParams(BMP280_Params_t *BMP280_Params);
+
+
+/**
+ * @fn      void BMP280_Init(BMP280_HandleTypeDef *BMP280)
+ * @brief   Uploads BMP280 configuration parameters into the BMP280 and start BMP280
+ * @param   *BMP280
+ * @return  None
+ */
+void BMP280_Init(BMP280_HandleTypeDef *BMP280);
+
+
+/**
+ * @fn       bool BMP280_Control_Status(BMP280_HandleTypeDef *BMP280)
+ * @brief    Checks the status register for operations of Copying value and Updating
+ * @param    *BMP280
+ * @return   boolean number according to sensor status
+ * @retval   '1' : The values are ready to get from BMP280
+ * 			 '0' : The values are not ready to get from BMP280 and it will wait until sensor is ready
+ */
+bool BMP280_Control_Status(BMP280_HandleTypeDef *BMP280);
+
+
+
+/**
+ * @fn      double BMP280_GetVal_TemperatureAndPressure(BMP280_HandleTypeDef *BMP280, double *BMP280_Temperature, double *BMP280_Pressure)
+ * @brief   Read rawValues for Temperature and Pressure from BMP280's chip registers.
+ * 			 And it is sent raw values to other function to calculate their real values
+ *
+ * @param   *BMP280
+ * @param   *BMP280_Temperature
+ * @param   *BMP280_Pressure
+ * @return  boolean number according to sensor status
+ * @retval  '1' : The values are ready to get from BMP280
+ * 			'0' : The values are not ready to get from BMP280 and it will wait until sensor is ready
+ */
+bool BMP280_GetVal_TemperatureAndPressure(BMP280_HandleTypeDef *BMP280, double *BMP280_Temperature, double *BMP280_Pressure, float *BMP280_Altitude);
+
+
+/**
+ * @fn      double BMP280_Calculate_CompensatedTemperature(BMP280_HandleTypeDef *BMP280, int32_t RawTemperature, int32_t *TempVarTotel)
+ * @brief   Calculate the Compensated Temperature by calibration datas
+ * @param   *BMP280
+ * @param   RawTemperature
+ * @param   *TempVarTotel
+ * @return 	Compensated Temperature
+ */
+double BMP280_Calculate_CompensatedTemperature(BMP280_HandleTypeDef *BMP280, int32_t RawTemperature, int32_t *TempVarTotel);
+
+/**
+ * @fn      double BMP280_Calculate_CompensatedPressure(BMP280_HandleTypeDef *BMP280, int32_t RawPressure,int32_t TempVarTotel)
+ * @brief   Calculate the Compensated Pressure by calibration datas
+ * @param   *BMP280
+ * @param   RawPressure
+ * @param   TempVarTotel
+ * @return  Compensated Pressure
+ */
+double BMP280_Calculate_CompensatedPressure(BMP280_HandleTypeDef *BMP280, int32_t RawPressure, int32_t TempVarTotel);
+
+/**
+ * @fn      float BMP280_Calculate_Altitude(double *BMP280_Pressure)
+ * @brief   Calculate the Altitude by BMP280_Pressure
+ * @param   *BMP280_Pressure
+ * @return  Altitude
+ */
+float BMP280_Calculate_Altitude(double *BMP280_Pressure);
+
+#endif /* INC_BMP280_H_ */
+
